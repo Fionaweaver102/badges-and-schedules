98cdd8159c1e1a17409e38e06aa9a510900b255e
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-10 12:51:52 -0700

    commit tree 217184c5b2cea463f1f4505619d2c44cc0465381

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |name| 
+        badges << "Hello, my name is #{name}."
+    end 
+    return badges
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    room = 1
+    attendees.each do |name| 
+        room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+        room+=1
+    end 
+    return room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees)
+        badges.each{|list| puts list}
+    assign_rooms(attendees)
+        room_assignments.each{|list| puts list}
+end
+
